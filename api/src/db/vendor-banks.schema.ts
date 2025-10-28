37a6d2aa7344a73a593a88bde6d6254a6451f339
krgyaan <gyanprakashk55@gmail.com>
2025-10-28 16:48:00 +0530

    feat(api): update journal and schema exports - Added new journal entry with version "7" and tag "0003_aberrant_gabe_jones".

---
+++ api/src/db/vendor-banks.schema.ts
@@ -0,0 +1,23 @@
+import {
+    pgTable,
+    bigserial,
+    varchar,
+    boolean,
+    timestamp,
+    bigint
+  } from 'drizzle-orm/pg-core';
+import { vendorOrganizations } from './vendor-organizations.schema';
+
+  export const vendorAccs = pgTable('vendor_accs', {
+    id: bigserial('id', { mode: 'number' }).primaryKey(),
+    org: bigint('org', { mode: 'number' }).notNull().references(()=>vendorOrganizations.id),
+    accountName: varchar('account_name', { length: 255 }).notNull(),
+    accountNum: varchar('account_num', { length: 255 }).notNull(),
+    accountIfsc: varchar('account_ifsc', { length: 255 }).notNull(),
+    status: boolean('status').notNull().default(true),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow(),
+  });
+
+  export type VendorAcc = typeof vendorAccs.$inferSelect;
+  export type NewVendorAcc = typeof vendorAccs.$inferInsert;
---
+++ api/src/db/vendor-gsts.schema.ts
@@ -0,0 +1,22 @@
+import {
+    pgTable,
+    bigserial,
+    varchar,
+    boolean,
+    timestamp,
+    bigint
+  } from 'drizzle-orm/pg-core';
+import { vendorOrganizations } from './vendor-organizations.schema';
+
+  export const vendorGsts = pgTable('vendor_gsts', {
+    id: bigserial('id', { mode: 'number' }).primaryKey(),
+    org: bigint('org', { mode: 'number' }).notNull().references(()=>vendorOrganizations.id),
+    gstState: varchar('gst_state', { length: 255 }).notNull(),
+    gstNum: varchar('gst_num', { length: 255 }).notNull(),
+    status: boolean('status').notNull().default(true),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow(),
+  });
+
+  export type VendorGst = typeof vendorGsts.$inferSelect;
+  export type NewVendorGst = typeof vendorGsts.$inferInsert;
