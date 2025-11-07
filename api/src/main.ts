734372cb26706d840c1a3be1e5e320fa24c61d2c
krgyaan <gyanprakashk55@gmail.com>
2025-11-07 10:52:23 +0530

    updated for production

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,30 +4,40 @@ import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = app.get(ConfigService);
-  const appCfg = config.get<{ port: number; apiPrefix: string }>('app', {
-    infer: true,
-  });
+    const config = app.get(ConfigService);
+    const appCfg = config.get<{ port: number; apiPrefix: string }>('app', {
+        infer: true,
+    });
 
-  if (appCfg?.apiPrefix) {
-    app.setGlobalPrefix(appCfg.apiPrefix);
-  }
+    if (appCfg?.apiPrefix) {
+        app.setGlobalPrefix(appCfg.apiPrefix);
+    }
 
     // Enable cookie parser
     app.use(cookieParser());
 
     app.setGlobalPrefix('api/v1');
 
+    const allowedOrigins = [
+        'http://localhost:5173',                 // local dev
+        'https://tmsv2.volksenergie.in',        // production frontend
+    ];
     app.enableCors({
-        origin: 'http://localhost:5173', // Frontend URL
-        credentials: true, // Important: Allow cookies
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+            callback(new Error('Not allowed by CORS'));
+        },
+        credentials: true,
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
-      });
+    });
 
-  await app.listen(appCfg?.port ?? 3000);
-  console.log(`App is Running on: http://localhost:${appCfg?.port ?? 3000}`);
+    await app.listen(appCfg?.port ?? 3000);
+    console.log(`App is Running on: http://localhost:${appCfg?.port ?? 3000}`);
 }
 bootstrap();
