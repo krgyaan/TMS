07ceb1b0b323975fb637d569781e81138b1eefd2
abhijeet <abhijeetgaur.dev@gmail.com>
2025-12-16 21:05:41 +0530

    feat: update createImprestSchema to use camelCase for field names and ensure amount is an integer

--- web/src/modules/shared/imprest/imprest.schema.ts
+++ web/src/modules/shared/imprest/imprest.schema.ts
@@ -1,12 +1,17 @@
 import * as z from "zod";
 
 export const createImprestSchema = z.object({
-    party_name: z.string().optional().nullable(),
-    project_name: z.string().optional().nullable(),
-    amount: z.number().min(1, "Amount must be at least 1"),
-    category: z.string().optional().nullable(),
-    team_id: z.number().optional().nullable(),
+    // strings
+    partyName: z.string().optional().nullable(),
+    projectName: z.string().optional().nullable(),
     remark: z.string().optional().nullable(),
+
+    // relations
+    categoryId: z.number().optional().nullable(),
+    teamId: z.number().optional().nullable(),
+
+    // numeric
+    amount: z.number().int().min(1, "Amount must be at least 1"),
 });
 
 export type CreateImprestInput = z.infer<typeof createImprestSchema>;
