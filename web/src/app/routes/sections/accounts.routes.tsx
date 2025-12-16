9189eaa5d6ee3b1dc9eafbb527a84f0f1b130afa
abhijeet <abhijeetgaur.dev@gmail.com>
2025-12-16 21:25:52 +0530

    feat: add ImprestPaymentHistory for imprest admin route and improve route structure in AccountsRoutes

--- web/src/app/routes/sections/accounts.routes.tsx
+++ web/src/app/routes/sections/accounts.routes.tsx
@@ -1,23 +1,69 @@
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { RouteWrapper } from '../components/RouteWrapper';
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import { RouteWrapper } from "../components/RouteWrapper";
+import ImprestPaymentHistory from "@/modules/accounts/imprest/ImprestPaymentHistory";
 
-const Accounts_Imprests = lazy(() => import('@/modules/accounts/imprests'));
-const Accounts_FinancialDocs = lazy(() => import('@/modules/accounts/financial-docs'));
-const Accounts_LoanAdvances = lazy(() => import('@/modules/accounts/loan-advances'));
-const Accounts_TaskChecklists = lazy(() => import('@/modules/accounts/task-checlkists'));
-const Accounts_GSTChecklists = lazy(() => import('@/modules/accounts/gst-checklists'));
-const Accounts_FixedExpenses = lazy(() => import('@/modules/accounts/fixed-expenses'));
+const Accounts_Imprests = lazy(() => import("@/modules/accounts/imprest/ImprestAdminIndex"));
+const Accounts_FinancialDocs = lazy(() => import("@/modules/accounts/financial-docs"));
+const Accounts_LoanAdvances = lazy(() => import("@/modules/accounts/loan-advances"));
+const Accounts_TaskChecklists = lazy(() => import("@/modules/accounts/task-checlkists"));
+const Accounts_GSTChecklists = lazy(() => import("@/modules/accounts/gst-checklists"));
+const Accounts_FixedExpenses = lazy(() => import("@/modules/accounts/fixed-expenses"));
 
 export default function AccountsRoutes() {
     return (
         <Routes>
-            <Route path="imprests" element={<RouteWrapper><Accounts_Imprests /></RouteWrapper>} />
-            <Route path="financial-docs" element={<RouteWrapper><Accounts_FinancialDocs /></RouteWrapper>} />
-            <Route path="loan-advances" element={<RouteWrapper><Accounts_LoanAdvances /></RouteWrapper>} />
-            <Route path="account-checklists" element={<RouteWrapper><Accounts_TaskChecklists /></RouteWrapper>} />
-            <Route path="gst-checklists" element={<RouteWrapper><Accounts_GSTChecklists /></RouteWrapper>} />
-            <Route path="fixed-expenses" element={<RouteWrapper><Accounts_FixedExpenses /></RouteWrapper>} />
+            <Route
+                path="imprests"
+                element={
+                    <RouteWrapper>
+                        <Accounts_Imprests />
+                    </RouteWrapper>
+                }
+            />
+            <Route path="imprest/payment-history/:userId" element={<ImprestPaymentHistory />} />
+            {/* <Route path="imprests/payment-history" element={<ImprestPaymentHistoryPage />} />
+            <Route path="imprests/voucher" element={<ImprestVoucherPage />} /> */}
+            <Route
+                path="financial-docs"
+                element={
+                    <RouteWrapper>
+                        <Accounts_FinancialDocs />
+                    </RouteWrapper>
+                }
+            />
+            <Route
+                path="loan-advances"
+                element={
+                    <RouteWrapper>
+                        <Accounts_LoanAdvances />
+                    </RouteWrapper>
+                }
+            />
+            <Route
+                path="account-checklists"
+                element={
+                    <RouteWrapper>
+                        <Accounts_TaskChecklists />
+                    </RouteWrapper>
+                }
+            />
+            <Route
+                path="gst-checklists"
+                element={
+                    <RouteWrapper>
+                        <Accounts_GSTChecklists />
+                    </RouteWrapper>
+                }
+            />
+            <Route
+                path="fixed-expenses"
+                element={
+                    <RouteWrapper>
+                        <Accounts_FixedExpenses />
+                    </RouteWrapper>
+                }
+            />
         </Routes>
     );
 }
