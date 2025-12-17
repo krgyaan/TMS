f5e64d16922c9026cce819b6757754a3c4ae01bf
abhijeet <abhijeetgaur.dev@gmail.com>
2025-12-17 14:21:59 +0530

    refactor: standardize string quotes and enhance follow-up relations in usersRelations

--- api/src/db/schemas/auth/users.relations.ts
+++ api/src/db/schemas/auth/users.relations.ts
@@ -1,17 +1,25 @@
-import { relations } from 'drizzle-orm';
-import { users } from '@db/schemas/auth/users.schema';
-import { userProfiles } from '@db/schemas/auth/user-profiles.schema';
-import { oauthAccounts } from '@db/schemas/auth/oauth-accounts.schema';
-import { tenderInfos } from '@db/schemas/tendering/tenders.schema';
-import { tenderStatusHistory } from '@db/schemas/tendering/tender-status-history.schema';
+import { relations } from "drizzle-orm";
+import { users } from "@db/schemas/auth/users.schema";
+import { userProfiles } from "@db/schemas/auth/user-profiles.schema";
+import { oauthAccounts } from "@db/schemas/auth/oauth-accounts.schema";
+import { tenderInfos } from "@db/schemas/tendering/tenders.schema";
+import { tenderStatusHistory } from "@db/schemas/tendering/tender-status-history.schema";
+import { followUps } from "../shared";
 
 export const usersRelations = relations(users, ({ many }) => ({
     userProfiles: many(userProfiles),
     oauthAccounts: many(oauthAccounts),
     tenderInfos: many(tenderInfos, {
-        relationName: 'teamMember',
+        relationName: "teamMember",
     }),
     tenderStatusHistory: many(tenderStatusHistory, {
-        relationName: 'changedBy',
+        relationName: "changedBy",
+    }),
+    assignedFollowUps: many(followUps, {
+        relationName: "assignedTo",
+    }),
+
+    createdFollowUps: many(followUps, {
+        relationName: "createdBy",
     }),
 }));
