317b4920e4054884cc139400f62cdf3b972c6364
abhijeet <abhijeetgaur.dev@gmail.com>
2025-12-19 19:05:41 +0530

    refactor: enhance CORS configuration to dynamically validate allowed origins

--- api/src/main.ts
+++ api/src/main.ts
@@ -27,9 +27,18 @@ async function bootstrap() {
     const allowedOrigins = ["http://localhost:5173", "https://tmsv2.volksenergie.in"];
 
     app.enableCors({
-        origin: allowedOrigins,
+        origin: function (origin: string, callback: (arg0: Error | null, arg1: boolean | undefined) => void) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+            callback(new Error("Not allowed by CORS"), true);
+        },
         credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     });
+
     await app.listen(appCfg?.port ?? 3000);
     console.log("GOOGLE_REDIRECT_URI:", process.env.GOOGLE_REDIRECT_URI);
     console.log(`App is Running on: http://localhost:${appCfg?.port ?? 3000}`);
