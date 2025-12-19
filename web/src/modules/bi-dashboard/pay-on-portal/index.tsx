174fee2aeb30163f9035e96a1f9195b670b0417e
krgyaan <gyanprakashk55@gmail.com>
2025-12-19 19:42:28 +0530

    refactor: remove 'company' column from various data tables to streamline data presentation

--- web/src/modules/bi-dashboard/pay-on-portal/index.tsx
+++ web/src/modules/bi-dashboard/pay-on-portal/index.tsx
@@ -2,12 +2,11 @@ import { useState } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import type { ColDef, RowSelectionOptions } from "ag-grid-community";
 import { useFetchJson } from "@/hooks/usFetchJson";
-import { dateCol, currencyCol, logoCol, booleanIconCol } from "@/components/data-grid";
+import { dateCol, currencyCol, booleanIconCol } from "@/components/data-grid";
 import DataTable from "@/components/ui/data-table";
 
 interface IRow {
   mission: string;
-  company: string;
   location: string;
   date: string;
   time: string;
@@ -28,8 +27,6 @@ const index = () => {
 
   const [colDefs] = useState<ColDef[]>([
     { field: "mission", width: 150 },
-    logoCol("company", { width: 130 }),
-    { field: "location", width: 225 },
     dateCol("date"),
     currencyCol(
       "price",
@@ -60,7 +57,6 @@ const index = () => {
           enableRowSelection={true}
           selectionType="multiple"
           onSelectionChanged={(rows) => console.log("Row Selected!", rows)}
-          height="100%"
         />
       </CardContent>
     </Card>
