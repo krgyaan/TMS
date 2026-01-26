7f40e2cdc4b23274d45f879fa4b06a4ef5331903
abhijeet <abhijeetgaur.dev@gmail.com>
2026-01-27 00:43:10 +0530

    vendor banks schema updated

--- api/src/db/schemas/vendors/vendor-banks.schema.ts
+++ api/src/db/schemas/vendors/vendor-banks.schema.ts
@@ -1,23 +1,17 @@
-import {
-    pgTable,
-    bigserial,
-    varchar,
-    boolean,
-    timestamp,
-    bigint
-  } from 'drizzle-orm/pg-core';
-import { vendorOrganizations } from './vendor-organizations.schema';
+import { pgTable, bigint, varchar, timestamp } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
-  export const vendorAccs = pgTable('vendor_accs', {
-    id: bigserial('id', { mode: 'number' }).primaryKey(),
-    org: bigint('org', { mode: 'number' }).notNull(),
-    accountName: varchar('account_name', { length: 255 }).notNull(),
-    accountNum: varchar('account_num', { length: 255 }).notNull(),
-    accountIfsc: varchar('account_ifsc', { length: 255 }).notNull(),
-    status: boolean('status').notNull().default(true),
-    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
-    updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow(),
-  });
+export const vendorAccs = pgTable("vendor_accs", {
+    id: bigint("id", { mode: "number" })
+        .primaryKey()
+        .default(sql`nextval('vendor_accs_id_seq')`),
+    orgId: bigint("org_id", { mode: "number" }).notNull(),
+    bankAccountName: varchar("bank_account_name", { length: 255 }).notNull(),
+    accountNum: varchar("account_num", { length: 255 }).notNull(),
+    ifscCode: varchar("ifsc_code", { length: 255 }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true }),
+    updatedAt: timestamp("updated_at", { withTimezone: true }),
+});
 
-  export type VendorAcc = typeof vendorAccs.$inferSelect;
-  export type NewVendorAcc = typeof vendorAccs.$inferInsert;
+export type VendorAcc = typeof vendorAccs.$inferSelect;
+export type NewVendorAcc = typeof vendorAccs.$inferInsert;
