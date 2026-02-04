674a0efc7e58e527bf5018e60364150c501f2829
abhijeet <abhijeetgaur.dev@gmail.com>
2026-02-05 02:19:57 +0530

    follow up worker module logging updated

--- api/src/modules/follow-up/followup-worker.module.ts
+++ api/src/modules/follow-up/followup-worker.module.ts
@@ -6,21 +6,10 @@ import { MailerModule } from "@/mailer/mailer.module";
 import { FollowUpModule } from "./follow-up.module";
 import { FollowupWorker } from "./follow-up.worker";
 import { FollowUpService } from "./follow-up.service";
-import { WinstonModule } from "nest-winston";
-import { winstonLogger } from "@/logger/logger.config";
+import { LoggerModule } from "@/logger/logger.module";
 
 @Module({
-    imports: [
-        ConfigModule,
-        DatabaseModule,
-        GoogleIntegrationModule,
-        MailerModule,
-        WinstonModule.forRoot({
-            transports: winstonLogger.transports,
-            format: winstonLogger.format,
-            level: winstonLogger.level,
-        }),
-    ],
+    imports: [ConfigModule, DatabaseModule, GoogleIntegrationModule, MailerModule, LoggerModule],
     providers: [FollowupWorker, FollowUpService],
 })
 export class FollowupWorkerModule {}
