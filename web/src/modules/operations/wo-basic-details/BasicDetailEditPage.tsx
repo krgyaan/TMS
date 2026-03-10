4fa1045215f6bbfa9427b9ddce364c687df16699
krgyaan <gyanprakashk55@gmail.com>
2026-03-10 13:35:50 +0530

    created Basic Details Frontend files

---
+++ web/src/modules/operations/wo-basic-details/BasicDetailEditPage.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+const BasicDetailEditPage = () => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Work Orders</CardTitle>
+                <CardDescription>All work orders listed</CardDescription>
+            </CardHeader>
+            <CardContent className="h-screen px-0">
+
+            </CardContent>
+        </Card>
+    );
+};
+
+export default BasicDetailEditPage
