19ed44dd6a48ba9a349f9193e1d992e7f802f4b9
abhijeet <abhijeetgaur.dev@gmail.com>
2026-04-03 19:14:57 +0530

    testing mails

--- api/src/modules/accounts/account-checklist/account-checklist.scheduler.ts
+++ api/src/modules/accounts/account-checklist/account-checklist.scheduler.ts
@@ -28,7 +28,7 @@ export class AccountChecklistScheduler {
     }
 
     // app:send-eod-checklist-mail — Mon–Fri 20:10
-    @Cron("10 20 * * 1-5")
+    @Cron("20 19 * * 1-5")
     async handleEodMails() {
         this.logger.info("Cron tick at enqueueEodMails", { timestamp: new Date() });
         try {
