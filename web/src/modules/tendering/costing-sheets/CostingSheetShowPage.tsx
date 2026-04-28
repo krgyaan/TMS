d41e5db6ccc8c0a367425a5cb15bcd8f2e39406c
krgyaan <gyanprakashk55@gmail.com>
2026-04-28 13:46:19 +0530

    Updated the following CostingSheetShowPage to include all core sections

--- web/src/modules/tendering/costing-sheets/CostingSheetShowPage.tsx
+++ web/src/modules/tendering/costing-sheets/CostingSheetShowPage.tsx
@@ -8,11 +8,14 @@ import { EmdTenderFeeSection } from "@/modules/tendering/emds-tenderfees/compone
 import { DocumentChecklistSection } from "@/modules/tendering/checklists/components/DocumentChecklistView";
 import { CostingSheetSection } from "./components/CostingSheetView";
 import { BidSubmissionSection } from "@/modules/tendering/bid-submissions/components/BidSubmissionView";
+import { RaSection } from "@/modules/tendering/ras/components/RaShow";
+import { TqTenderSection } from "@/modules/tendering/tq-management/components/TqView";
+import { TenderResultSection } from "@/modules/tendering/results/components/TenderResultShow";
 import { paths } from "@/app/routes/paths";
 import { ShowPageLayout } from "@/components/layout/ShowPageLayout";
 
-// CostingSheet page shows all steps EXCEPT "result"
-const STEP_IDS = ["tender-details", "physical-docs", "rfq", "emd-fees", "checklist", "costing", "bid"];
+// CostingSheet page shows all relevant steps
+const STEP_IDS = ["tender-details", "physical-docs", "rfq", "emd-fees", "checklist", "costing", "bid", "tq-management", "ra-management", "result"];
 
 export default function CostingSheetShowPage() {
     const { tenderId: tenderIdParam } = useParams<{ tenderId: string }>();
@@ -37,12 +40,15 @@ export default function CostingSheetShowPage() {
     const renderSectionContent = useCallback((stepId: string) => {
         switch (stepId) {
             case "tender-details": return <TenderDetailsSection tenderId={tenderId} />;
-            case "physical-docs":  return <PhysicalDocsSection  tenderId={tenderId} />;
-            case "rfq":            return <RfqSection           tenderId={tenderId} />;
-            case "emd-fees":       return <EmdTenderFeeSection  tenderId={tenderId} />;
+            case "physical-docs":  return <PhysicalDocsSection tenderId={tenderId} />;
+            case "rfq":            return <RfqSection tenderId={tenderId} />;
+            case "emd-fees":       return <EmdTenderFeeSection tenderId={tenderId} />;
             case "checklist":      return <DocumentChecklistSection tenderId={tenderId} />;
-            case "costing":        return <CostingSheetSection  tenderId={tenderId} />;
-            case "bid":            return <BidSubmissionSection  tenderId={tenderId} />;
+            case "costing":        return <CostingSheetSection tenderId={tenderId} />;
+            case "bid":            return <BidSubmissionSection tenderId={tenderId} />;
+            case "tq-management":  return <TqTenderSection tenderId={tenderId} />;
+            case "ra-management":  return <RaSection tenderId={tenderId!} />;
+            case "result":         return <TenderResultSection tenderId={tenderId} />;
             default:               return null;
         }
     }, [tenderId]);
