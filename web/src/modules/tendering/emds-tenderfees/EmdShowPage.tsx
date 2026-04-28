5234bf036da46aea775bfefadaaecccd12c23ce6
krgyaan <gyanprakashk55@gmail.com>
2026-04-28 13:46:37 +0530

    Updated the following EmdShowPage to include all core sections

--- web/src/modules/tendering/emds-tenderfees/EmdShowPage.tsx
+++ web/src/modules/tendering/emds-tenderfees/EmdShowPage.tsx
@@ -3,7 +3,14 @@ import { useState, useCallback } from "react";
 import { paths } from "@/app/routes/paths";
 import { TenderDetailsSection } from "@/modules/tendering/tenders/components/TenderView";
 import { PhysicalDocsSection } from "@/modules/tendering/physical-docs/components/PhysicalDocsView";
+import { RfqSection } from "@/modules/tendering/rfqs/components/RfqView";
 import { EmdTenderFeeSection } from "@/modules/tendering/emds-tenderfees/components/EmdTenderFeeShow";
+import { DocumentChecklistSection } from "@/modules/tendering/checklists/components/DocumentChecklistView";
+import { CostingSheetSection } from "@/modules/tendering/costing-sheets/components/CostingSheetView";
+import { BidSubmissionSection } from "@/modules/tendering/bid-submissions/components/BidSubmissionView";
+import { RaSection } from "@/modules/tendering/ras/components/RaShow";
+import { TqTenderSection } from "@/modules/tendering/tq-management/components/TqView";
+import { TenderResultSection } from "@/modules/tendering/results/components/TenderResultShow";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { AlertCircle } from "lucide-react";
 import { ShowPageLayout } from "@/components/layout/ShowPageLayout";
@@ -35,7 +42,14 @@ export default function EmdShowPage() {
         switch (stepId) {
             case "tender-details":   return <TenderDetailsSection tenderId={tenderId} />;
             case "physical-docs":    return <PhysicalDocsSection tenderId={tenderId} />;
+            case "rfq":              return <RfqSection tenderId={tenderId} />;
             case "emd-fees":         return <EmdTenderFeeSection tenderId={tenderId} />;
+            case "checklist":        return <DocumentChecklistSection tenderId={tenderId} />;
+            case "costing":          return <CostingSheetSection tenderId={tenderId} />;
+            case "bid":              return <BidSubmissionSection tenderId={tenderId} />;
+            case "tq-management":    return <TqTenderSection tenderId={tenderId} />;
+            case "ra-management":    return <RaSection tenderId={tenderId} />;
+            case "result":           return <TenderResultSection tenderId={tenderId} />;
             default: return null;
         }
     };
@@ -51,7 +65,7 @@ export default function EmdShowPage() {
 
     return (
         <ShowPageLayout
-            steps={tenderSteps.filter(s => ["tender-details", "physical-docs", "emd-fees"].includes(s.id))}
+            steps={tenderSteps.filter(s => ["tender-details", "physical-docs", "rfq", "emd-fees", "checklist", "costing", "bid", "tq-management", "ra-management", "result"].includes(s.id))}
             expandedSections={expandedSections}
             onToggleSection={toggleSection}
             onExpandAll={expandAll}
