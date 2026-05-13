e2d010cead581e93243645f216d3c1c140e89c34
abhijeet <abhijeetgaur.dev@gmail.com>
2026-05-13 14:00:24 +0530

    update info sheet filling logic for only info filled tenders

--- web/src/modules/tendering/tenders/TenderListPage.tsx
+++ web/src/modules/tendering/tenders/TenderListPage.tsx
@@ -52,6 +52,15 @@ const TenderListPage = () => {
         }  
     }
 
+    const elligibleForEditing = (row) => {
+        if([0,1,2].includes(row.status)){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
     const handleSortChanged = useCallback((event: any) => {
         const sortModel = event.api.getColumnState()
             .filter((col: any) => col.sort)
@@ -144,12 +153,13 @@ const TenderListPage = () => {
             label: "Fill Info Sheet",
             onClick: (row: TenderWithRelations) => (row.infoSheet ? navigate(paths.tendering.infoSheetEdit(row.id)) : navigate(paths.tendering.infoSheetCreate(row.id))),
             icon: <FilePlus className="h-4 w-4" />,
+            visible: (row) => elligibleForEditing(row), 
         },
         {
             label: "Reject Tender",
             onClick: (row) => setRejectionModal({ open: true, tenderId: row.id, tenderName: row.tenderName }),
             icon: <XCircle className="h-4 w-4" />,
-            visible:(row) => elligibleForRejection(row) ,
+            visible:(row) => elligibleForRejection(row) && hasTenderingPermission,
         },
         // {
         //     label: "Change Status",
