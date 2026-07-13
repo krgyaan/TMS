8281e9cea1b92b78c47f509e59832ed83c4de627
krgyaan <gyanprakashk55@gmail.com>
2026-07-13 13:15:23 +0530

    feat: configure global environment variables loading and validation for multiple modules

--- api/src/modules/follow-up/followup-worker.module.ts
+++ api/src/modules/follow-up/followup-worker.module.ts
@@ -1,11 +1,31 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import appConfig, { validateAppEnv } from "@/config/app.config";
+import authConfig, { validateAuthEnv } from "@/config/auth.config";
+import dbConfig, { validateDbEnv } from "@/config/db.config";
+import googleConfig, { validateGoogleEnv } from "@/config/google.config";
+import redisConfig, { validateRedisEnv } from "@/config/redis.config";
 import { FollowUpModule } from "./follow-up.module";
 import { FollowupWorker } from "./follow-up.worker";
 import { LoggerModule } from "@/logger/logger.module";
 
 @Module({
-    imports: [ConfigModule, FollowUpModule, LoggerModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            expandVariables: true,
+            load: [appConfig, dbConfig, googleConfig, authConfig, redisConfig],
+            validate: env => ({
+                ...validateAppEnv(env),
+                ...validateDbEnv(env),
+                ...validateGoogleEnv(env),
+                ...validateAuthEnv(env),
+                ...validateRedisEnv(env),
+            }),
+        }),
+        FollowUpModule,
+        LoggerModule,
+    ],
     providers: [FollowupWorker],
 })
 export class FollowupWorkerModule {}
