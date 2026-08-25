bfd57f722599f02dc453d86254c9f1da509e99db
krgyaan <gyanprakashk55@gmail.com>
2026-08-25 11:42:29 +0530

    fix(physical-docs): improve error logging for mail sending failure

--- api/src/modules/tendering/physical-docs/physical-docs.service.ts
+++ api/src/modules/tendering/physical-docs/physical-docs.service.ts
@@ -597,7 +597,7 @@ export class PhysicalDocsService {
 
             this.logger.debug("mail completed successfully");
         } catch (err) {
-            this.logger.error("Mail sending failed", err);
+            this.logger.error("Mail sending failed");
         }
 
         //STOPPING TIMER
